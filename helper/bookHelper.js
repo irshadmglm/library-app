eaccdbeec530254533c9b3cd19d867c6ca13d6ba
irshad <irshadmglm@gmail.com>
2024-12-04 23:53:41 +0530

    Fix: Updated configuration for Render deployment

--- helper/bookHelper.js
+++ helper/bookHelper.js
@@ -13,7 +13,6 @@ module.exports = {
     getBooks:function(){
         return new Promise(async(resolve,reject)=>{
             try {
-                this.exeldata()
                let books = await db.get().collection(collections.BOOK_COLLECTION).find().sort({bookNumber: 1}).toArray()
                resolve(books);
             } catch (error) {
